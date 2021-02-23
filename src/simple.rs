b589164b485d651ae5fafa6ee07c867fe7d82539
LighghtEeloo <2415467501@qq.com>
2021-02-23 17:12:00 +0800

    - fix: add simple unsafe.

--- src/simple.rs
+++ src/simple.rs
@@ -9,6 +9,14 @@ pub fn main() {
     // let p = &mut a;
     // a = 2;
     // *p = 3;
+
+    //// but this can:
+    let mut a = 1;
+    let p: *mut i32 = &mut a;
+    unsafe {
+        a = 2;
+        *p = 3;
+    }
 }
 
 fn borrow_and_return(string: &String) -> &String {
