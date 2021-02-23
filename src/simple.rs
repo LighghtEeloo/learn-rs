3ab0a22378ec279db22356a9ac95de2237b52c52
LighghtEeloo <2415467501@qq.com>
2021-02-23 17:05:27 +0800

    - fix: "no double &mut" example in simple.

--- src/simple.rs
+++ src/simple.rs
@@ -3,6 +3,12 @@ pub fn main() {
     let string = String::from("Hello.");
     borrow_and_return(&string);
     println!("{}", string);
+
+    //// won't compile.
+    // let mut a = 1;
+    // let p = &mut a;
+    // a = 2;
+    // *p = 3;
 }
 
 fn borrow_and_return(string: &String) -> &String {
